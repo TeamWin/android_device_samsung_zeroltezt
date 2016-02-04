4f9cc2008939255917b04532571cd9fd2e75dce1
jenkins <jenkins@teamw.in>
2016-02-04 18:53:51 -0500

    Fix graphics

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,20 +32,20 @@ TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-vBPRE
 
 # Kernel
-#KERNEL_TOOLCHAIN := /builds/cm-12.1/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
-#KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
-#BOARD_KERNEL_IMAGE_NAME := Image
+#TARGET_KERNEL_CUSTOM_TOOLCHAIN := true
+#ARM_EABI_TOOLCHAIN := /builds/omni-5.1/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_MKBOOTIMG_ARGS := --dt $(LOCAL_PATH)/dt.img
 BOARD_KERNEL_PAGESIZE := 2048
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_USES_UNCOMPRESSED_KERNEL := true
-#TARGET_KERNEL_SOURCE := kernel/samsung/exynos7420
-#TARGET_KERNEL_CONFIG := exynos7420-zerolte_tmo_defconfig
+#TARGET_KERNEL_SOURCE := kernel/samsung/zerolte
+#TARGET_KERNEL_CONFIG := exynos7420-zerolte_defconfig
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernAl
 
-TARGET_RECOVERY_INITRC := device/samsung/zeroltetmo/init.rc
+TARGET_RECOVERY_INITRC := device/samsung/zerolte/init.rc
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 24164353
@@ -56,7 +56,7 @@ BOARD_FLASH_BLOCK_SIZE := 2048
 
 # SELinux
 BOARD_SEPOLICY_DIRS += \
-	device/samsung/zeroltetmo/sepolicy
+	device/samsung/zeroltezt/sepolicy
 
 BOARD_SEPOLICY_UNION += \
 	file_contexts \
@@ -93,5 +93,7 @@ TW_BRIGHTNESS_PATH := "/sys/devices/13900000.dsim/backlight/panel/brightness"
 TW_MAX_BRIGHTNESS := 255
 TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
-TW_MTP_DEVICE := /dev/usb_mtp_gadget
+TW_MTP_DEVICE := /dev/mtp_usb
 TW_SCREEN_BLANK_ON_BOOT := true
+TW_MTP_SAMSUNG := true
+TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
