bd5144da3c3041535fa9cbb90d2be3e96a62c86a
James Christopher Adduono <jc@adduono.com>
2016-04-06 03:19:05 -0400

    Initialize device tree for zeroltezt

---
+++ omni_zeroltezt.mk
@@ -0,0 +1,14 @@
+# Release name
+PRODUCT_RELEASE_NAME := zeroltezt
+
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := zeroltezt
+PRODUCT_NAME := omni_zeroltezt
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-G9250
+PRODUCT_MANUFACTURER := samsung
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo omni_zeroltezt-eng
+add_lunch_combo omni_zeroltezt-userdebug
